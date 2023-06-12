1260a938c7539aa9cdac93b2b317a60c06501ae4
Dom <domeccleston@gmail.com>
2023-06-12 10:30:27 +0100

    avoid top level await

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@ import { OpenAI } from "langchain";
 
 dotenv.config();
 
-const model = new OpenAI({
-  modelName: "gpt-3.5-turbo",
-  openAIApiKey: process.env.OPENAI_API_KEY,
-});
+async function main() {
+  const model = new OpenAI({
+    modelName: "gpt-3.5-turbo",
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
 
-const res = await model.call(
-  "What's a good idea for an application to build with GPT-3?"
-);
+  const res = await model.call(
+    "What's a good idea for an application to build with GPT-3?"
+  );
 
-console.log(res);
+  console.log(res);
+}
+
+main();
